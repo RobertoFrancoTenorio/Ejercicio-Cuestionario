9960cf4d57509befc36d808db5d0804aa88ccdaf
Roberto Carlos Franco Tenorio <robertofrancotenorio2@gmail.com>
2017-09-11 11:11:08 -0500

    Initial sharing of project

---
+++ Pregunta.java
@@ -0,0 +1,21 @@
+public class Pregunta
+{
+    private String texto;
+    private String respuesta;
+    
+    public Pregunta()
+    {
+        
+    }
+    
+    public String asignarTexto(String pregunta)
+    {
+        texto = pregunta;
+        return (pregunta);
+    }
+    
+    public boolean checarRespuesta(String resp)
+    {
+        
+    }
+}
